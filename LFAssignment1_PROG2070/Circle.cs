20f6f6029ea41c0168cef0de1b19b322914f0387
lfeng4782 <phyflx@hotmail.com>
2019-01-29 09:24:01 -0500

    Completed Circle class

---
+++ LFAssignment1_PROG2070/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFAssignment1_PROG2070
+{
+    public class Circle
+    {
+        private int radius;
+
+        // defaut constructor
+        public Circle()
+        {
+            radius = 1;
+        }
+        //constructor
+        public Circle(int radius)
+        {            
+            this.radius = radius;
+        }
+        //methods
+        public int GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
+        }
+        public double GetCircumference()
+        {
+            return Math.Round(2 * Math.PI * radius, 3);
+        }
+        public double GetArea()
+        {
+            return Math.Round(Math.PI * radius * radius, 3);
+        }
+    }
+}
